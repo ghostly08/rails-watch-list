4ed69eea60253c36d9a0b99df037a7912df203a6
ghostly08 <steven.produccionbcn@gmail.com>
2022-02-17 15:16:10 +0100

    creating seeds

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,6 +2,6 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
 
-  validates :comment, length: { minimun: 6 }
+  validates :comment, length: { minimum: 6 }
   validates :movie_id, uniqueness: { scope: :list_id }
 end
